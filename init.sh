11d4b977da866fdb2c6a80a830cbe9ec9c7bd4a6
andrewzn <ondrejzeman23@gmail.com>
2023-05-29 08:45:59 +0200

    feat(init): add section for installing composer and grav

--- init.sh
+++ init.sh
@@ -18,7 +18,20 @@ apt update
 apt upgrade -y
 
 # install necessary packages
-apt install -y nginx php certbot python3-certbot-nginx
+apt install -y nginx php php-cli certbot python3-certbot-nginx unzip
+
+# install composer
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+# install grav
+git clone -b master https://github.com/getgrav/grav.git
+cd grav
+composer install --no-dev -o
+bin/grav install
+
 
 # start and enable nginx
 systemctl start nginx
